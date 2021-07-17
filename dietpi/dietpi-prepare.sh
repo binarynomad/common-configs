dd003ab59af31d86b94b22bb01e7d3d8c01a3a96
binarynomad <binarynoamd@gmail.com>
2021-07-17 11:52:40 -0700

    adding the dietpi script

---
+++ dietpi/dietpi-prepare.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# ----------------------------------------------------------------------------
+#        name : programName
+#      author : "Brian Huddleston" <binarynomad@gmail.com>
+# create_date : 2021-07-17 (11:51)
+# description : script to perform initial setup of a dietpi that is not able to be automated by the dietpi.txt file.
+# ----------------------------------------------------------------------------
+
+## DRAFT ##
+# https://devhints.io/bash
+#
+#
+
+# -------------------------------------
+# Functions
+# -------------------------------------
+
+
+
+# -------------------------------------
+# Main Program
+# -------------------------------------
+
+
+
+
