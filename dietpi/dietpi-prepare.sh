12ade3176874572bae40055d44d7e9639bacc073
binarynomad <binarynoamd@gmail.com>
2021-07-17 22:43:41 -0700

    added check for resetting hostkeys

--- dietpi/dietpi-prepare.sh
+++ dietpi/dietpi-prepare.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # ----------------------------------------------------------------------------
-#        name : programName
+#        name : dietpi-prepare.sh
 #      author : "Brian Huddleston" <binarynomad@gmail.com>
 # create_date : 2021-07-17 (11:51)
 # description : script to perform initial setup of a dietpi that is not able to be automated by the dietpi.txt file.
