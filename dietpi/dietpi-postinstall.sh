081f1fefb3b0d87366928192599692e9589e2493
binarynomad <binarynoamd@gmail.com>
2021-08-17 15:02:39 -0700

    renamed file to git-postinstall.sh

--- dietpi/dietpi-postinstall.sh
+++ dietpi/dietpi-postinstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # ----------------------------------------------------------------------------
-#        name : dietpi-prepare.sh
+#        name : dietpi-postinstall.sh
 #      author : "Brian Huddleston" <binarynomad@gmail.com>
 # create_date : 2021-07-17 (11:51)
 # description : script to perform initial setup of a dietpi that is not able to be automated by the dietpi.txt file.
